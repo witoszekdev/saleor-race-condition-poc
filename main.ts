903558ac7a5c12cfd40b2674448f8b61d6f89940
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-12-11 17:37:28 +0100

    Add script

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import {
+  gql,
+  request,
+} from "https://deno.land/x/graphql_request@v4.1.0/mod.ts";
+
+const SALEOR_API_URL =
+  Deno.env.get("SALEOR_API_URL") ?? "http://localhost:8000/graphql/";
+const NODE_ID = Deno.env.get("NODE_ID");
+const KEYS_TO_CREATE = 10;
+
+if (!NODE_ID) {
+  throw new Error("NODE_ID env var is not set");
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const mutation = gql`
+  mutation UpdateMetdata($id: ID!, $key: String!, $value: String!) {
+    updateMetadata(id: $id, input: { key: $key, value: $value }) {
+      item {
+        metafields
+      }
+    }
+  }
+`;
+
+const keys = [...Array(KEYS_TO_CREATE).keys()].map((i) => `key${i}`);
+
+const responses = await Promise.all(
+  keys.map(async (key) => {
+    return await request(SALEOR_API_URL, mutation, {
+      id: NODE_ID,
+      key,
+      value: `hello from script - ${key}`,
+    });
+  }),
+);
+
+console.log(responses);
